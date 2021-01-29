079129dcf8c3305e09e4d4e8fd2bccc603b3975b
bad5tuber <78054011+bad5tuber@users.noreply.github.com>
2021-01-28 18:56:55 -0800

    Update_Thru_Switch
    Updated through to switch statements

---
+++ MlerkinTheJerkinGherkin/Assets/Scripts/Movement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float mainRotation = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if(!audioSource.isPlaying) 
+            {
+            audioSource.Play();
+            }
+            
+        }
+        else 
+        {
+        audioSource.Stop();
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(mainRotation);
+        }
+
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-mainRotation);
+        }
+    }
+
+    // We've taken an expansive set of code and extracted a method that is being passed
+    // the parameter of rotationThisFrame. That is used in the codeblock and above, the
+    // value being passed into the method parameter is our mainRotation variable declared
+    // before the Start() method
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreezing rotation so the physics system can take over
+    }
+}
